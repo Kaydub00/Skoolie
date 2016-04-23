434e6177237176236e11dc3f55a37c078b59212a
kevinw <kevinwhite88@gmail.com>
2016-04-23 13:51:19 -0400

    Added interface and domain objects but no implementation of either Signed-off-by: kevinw <kevinwhite88@gmail.com>

---
+++ Skoolie-Parent/skoolie-business/src/main/java/com/dub/skoolie/business/service/schedule/templates/ScheduleTemplateGroupWizardService.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dub.skoolie.business.service.schedule.templates;
+
+import com.dub.skoolie.structures.schedule.templates.ScheduleTemplateGroupWizardBean;
+
+/**
+ *
+ * @author Kevin W
+ */
+public interface ScheduleTemplateGroupWizardService {
+    
+    public void setupSchedule(ScheduleTemplateGroupWizardBean bean);
+}
---
+++ Skoolie-Parent/skoolie-structures/src/main/java/com/dub/skoolie/structures/schedule/templates/ScheduleTemplateGroupWizardBean.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dub.skoolie.structures.schedule.templates;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Kevin W
+ */
+public class ScheduleTemplateGroupWizardBean implements Serializable {
+    
+}
