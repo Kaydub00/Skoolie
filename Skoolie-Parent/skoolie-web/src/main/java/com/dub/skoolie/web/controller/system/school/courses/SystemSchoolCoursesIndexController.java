d66fa487e798acde20a2e571945015ffc0c7065c
kevinw <kevinwhite88@gmail.com>
2016-08-06 18:01:16 -0400

    setting up forms for subjects and courses
    Signed-off-by: kevinw <kevinwhite88@gmail.com>

--- Skoolie-Parent/skoolie-web/src/main/java/com/dub/skoolie/web/controller/system/school/courses/SystemSchoolCoursesIndexController.java
+++ Skoolie-Parent/skoolie-web/src/main/java/com/dub/skoolie/web/controller/system/school/courses/SystemSchoolCoursesIndexController.java
@@ -5,6 +5,8 @@
  */
 package com.dub.skoolie.web.controller.system.school.courses;
 
+import com.dub.skoolie.structures.courses.CourseBean;
+import com.dub.skoolie.structures.courses.SubjectBean;
 import com.dub.skoolie.web.service.courses.UiCourseService;
 import com.dub.skoolie.web.service.courses.UiSubjectService;
 import com.dub.skoolie.web.service.school.UiSchoolService;
@@ -36,6 +38,8 @@ public class SystemSchoolCoursesIndexController {
     public ModelAndView getSchoolCoursesIndex(@PathVariable("id") Long school, Model model) {
         model.addAttribute("schoolBean", uiSchoolServiceImpl.getSchool(school));
         model.addAttribute("subjectBeans", uiSubjectServiceImpl.getSubjectsBySchool(school));
+        model.addAttribute("subjectBean", new SubjectBean());
+        model.addAttribute("courseBean", new CourseBean());
         model.addAttribute("courseBeans", uiCourseServiceImpl.getCoursesBySchool(school));
         return new ModelAndView("system/school/courses/index");
     }
