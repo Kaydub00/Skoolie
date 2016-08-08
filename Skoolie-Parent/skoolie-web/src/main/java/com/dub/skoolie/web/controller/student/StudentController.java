149b60eaf5bafb60cd575e76bb2dbe46b1342094
kevinw <kevinwhite88@gmail.com>
2016-08-08 08:12:53 -0400

    Setting up teacher and student routes so you can login as teacher or student
    Signed-off-by: kevinw <kevinwhite88@gmail.com>

--- Skoolie-Parent/skoolie-web/src/main/java/com/dub/skoolie/web/controller/student/StudentController.java
+++ Skoolie-Parent/skoolie-web/src/main/java/com/dub/skoolie/web/controller/student/StudentController.java
@@ -1,10 +1,21 @@
 
 package com.dub.skoolie.web.controller.student;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
 /**
  *
  * @author Kevin W.
  */
+@Controller
 public class StudentController {
+    
+    @RequestMapping("/student")
+    public ModelAndView index(Model model) {
+        return new ModelAndView("student/index");
+    }
 
 }
--- Skoolie-Parent/skoolie-web/src/main/java/com/dub/skoolie/web/controller/teacher/TeacherController.java
+++ Skoolie-Parent/skoolie-web/src/main/java/com/dub/skoolie/web/controller/teacher/TeacherController.java
@@ -1,10 +1,21 @@
 
 package com.dub.skoolie.web.controller.teacher;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
 /**
  *
  * @author Kevin W.
  */
+@Controller
 public class TeacherController {
+    
+    @RequestMapping("/teacher")
+    public ModelAndView index(Model model) {
+        return new ModelAndView("teacher/index");
+    }
 
 }
