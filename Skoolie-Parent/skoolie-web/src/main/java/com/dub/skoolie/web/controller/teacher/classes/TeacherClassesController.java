8336599f41d27bd44fb3b10f915e47262def8cc5
kevinw <kevinwhite88@gmail.com>
2016-09-11 13:42:25 -0400

    #32 still working on different teacher views
    Signed-off-by: kevinw <kevinwhite88@gmail.com>

--- Skoolie-Parent/skoolie-web/src/main/java/com/dub/skoolie/web/controller/teacher/classes/TeacherClassesController.java
+++ Skoolie-Parent/skoolie-web/src/main/java/com/dub/skoolie/web/controller/teacher/classes/TeacherClassesController.java
@@ -11,6 +11,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -31,5 +32,11 @@ public class TeacherClassesController {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         model.addAttribute("schoolClassBeans", uiSchoolClassServiceImpl.getTeacherCurrentClasses(auth.getName()));
         return new ModelAndView("teacher/classes/index");
+    }
+    
+    @RequestMapping(value="/teacher/classes/{id}", method=RequestMethod.GET)
+    public ModelAndView getTeacherClass(@PathVariable("id") Long schoolClass, Model model) {
+        model.addAttribute("schoolClassBean", uiSchoolClassServiceImpl.getSchoolClass(schoolClass));
+        return new ModelAndView("teacher/classes/class");
     }
 }
